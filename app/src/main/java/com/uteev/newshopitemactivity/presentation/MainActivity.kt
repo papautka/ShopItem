ec15dbcbf061f8d7d7c37efe583616e071aab600
andrew <120500uas@gmail.com>
2024-04-21 19:31:30 +0300

    connect_adapter

--- app/src/main/java/com/uteev/newshopitemactivity/presentation/MainActivity.kt
+++ app/src/main/java/com/uteev/newshopitemactivity/presentation/MainActivity.kt
@@ -7,43 +7,28 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.uteev.newshopitemactivity.R
 import com.uteev.newshopitemactivity.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel : MainViewModel
-    private lateinit var llShopList : LinearLayout
+    private lateinit var adapter: ShopListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupRecyclerView()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-        llShopList = findViewById(R.id.ll_shop_list)
         viewModel.shopList.observe(this) {
-            showList(it)
             // Perform any necessary actions with the shopList
+            adapter.shopList = it
         }
     }
-
-    private fun showList(list : List<ShopItem>) {
-        llShopList.removeAllViews()
-        for (shopItem in list) {
-           val layoutId = if(shopItem.enabled) {
-               R.layout.item_shop_enabled
-           } else {
-               R.layout.item_shop_disbaled
-           }
-           val view = LayoutInflater.from(this).inflate(layoutId, llShopList, false)
-            val tvName = view.findViewById<TextView>(R.id.tv_name)
-            val tvCount = view.findViewById<TextView>(R.id.tv_count)
-            tvName.text = shopItem.name
-            tvCount.text = shopItem.count.toString()
-            llShopList.addView(view)
-
-            view.setOnLongClickListener {
-                viewModel.editShopItem(shopItem)
-                true
-            }
+    private fun setupRecyclerView() {
+        val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
+        adapter = ShopListAdapter()
+        rvShopList.adapter = adapter
     }
-    }
+
 }
--- app/src/main/java/com/uteev/newshopitemactivity/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/uteev/newshopitemactivity/presentation/ShopListAdapter.kt
@@ -5,29 +5,36 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.uteev.newshopitemactivity.R
 import com.uteev.newshopitemactivity.domain.ShopItem
 
 class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
-    class ShopItemViewHolder(val view : View) : RecyclerView.ViewHolder(view) {
+    class ShopItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         val tvName = view.findViewById<TextView>(R.id.tv_name)
         val tvCount = view.findViewById<TextView>(R.id.tv_count)
     }
 
-    val list = listOf<ShopItem>()
+    var shopList = listOf<ShopItem>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
 
     override fun getItemCount(): Int {
-        return  list.size
+        return shopList.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_enabled, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_shop_enabled,
+            parent,
+            false
+        )
         return ShopItemViewHolder(view)
     }
 
     override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
-        val shopItem = list[position]
+        val shopItem = shopList[position]
         viewHolder.tvName.text = shopItem.name
         viewHolder.tvCount.text = shopItem.count.toString()
 
