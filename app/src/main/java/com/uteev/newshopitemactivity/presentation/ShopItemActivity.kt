59a32cd7f081bbe4d5a15b748ee89f3154282c87
andrew <120500uas@gmail.com>
2024-04-23 11:09:59 +0300

    glava_five_text_input_layout_xml

---
+++ app/src/main/java/com/uteev/newshopitemactivity/presentation/ShopItemActivity.kt
@@ -0,0 +1,13 @@
+package com.uteev.newshopitemactivity.presentation
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.uteev.newshopitemactivity.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
